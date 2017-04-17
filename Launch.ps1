bfa37aaf4e019453f8a0f4809456f2091a28504e
Agustin Berbegall <aberbegall@gmail.com>
2017-04-17 21:33:50 +0200

    Code refactored and added finally steps

--- Launch.ps1
+++ Launch.ps1
@@ -1,4 +1,4 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 Param
 (
     [Parameter(Mandatory=$True)]
@@ -20,36 +20,62 @@ function Find-ListeningPort([int] $port)
      return ($listening -ne $null)
 }
 
-function Set-VncWebRoot([string] $webpath)
+function Install-NodeJs()
 {
-    If ($webpath)
+    Write-Verbose "Installing nodejs"
+    choco install nodejs.install -y
+
+    Write-Verbose "Installing required nodejs modules"
+    npm install -g optimist, policyfile, ws, http-server
+
+    Write-Verbose "Configuring nodejs installation"
+    [Environment]::SetEnvironmentVariable("NODE_PATH", "%AppData%\npm\node_modules", "User")
+    [Environment]::SetEnvironmentVariable("Path", ("{0};{1}" -f [Environment]::GetEnvironmentVariable("Path","User"), [Environment]::GetEnvironmentVariable("NODE_PATH","User")), "User")    
+}
+
+function Get-NoVncSource([string] $tempfolder, [string] $novncfolder)
+{
+    Write-Verbose "Downloading novnc git master repository; $($tempfolder)\novnc.zip"    
+    wget https://github.com/novnc/noVNC/archive/master.zip -OutFile "$($tempfolder)\novnc.zip"
+    
+    Write-Verbose "Extracting novnc source in $($novncfolder)"
+    Add-Type -AssemblyName "System.IO.Compression.FileSystem"    
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("$($tempfolder)\novnc.zip", "$($webfolder)")
+}
+
+function Get-WebSockifySource([string] $tempfolder, [string] $websockifyfolder)
+{
+    Write-Verbose "Downloading websockify git master repository; $($tempfolder)\websockify.zip"
+    wget https://github.com/novnc/websockify/archive/master.zip -OutFile "$($tempfolder)\websockify.zip"
+
+    Write-Verbose "Extracting novnc source in $($websockifyfolder)"
+    Add-Type -AssemblyName "System.IO.Compression.FileSystem"    
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("$($tempfolder)\websockify.zip", "$($webfolder)")
+}
+
+function Start-NoVncWebServer([string] $novncfolder, [string] $webport)
+{
+    [string]$arguments = "http-server $($novncfolder) -p $($webport)"
+    Write-Verbose "Starting novnc web server on port $($webport)"
+    $novncprocess = Start-Process powershell -ArgumentList $arguments -PassThru
+    If ($novncprocess -eq $null)
     {
-        [string]$vncwebfile = "$($webpath)\vnc.html"
-        If (-Not (Test-Path $vncwebfile))
-        {
-            throw [System.IO.FileNotFoundException] "Could not find $($vncwebfile)"
-        }
+        throw "Failed to start novnc web server"
     }
-    ElseIf (Test-Path "$($PWD)\vnc.html")
+    return $novncprocess
+}
+
+function Start-WebSockifyProxy([string] $websockifyfolder, [string] $proxyport, [string] $vnc)
+{
+    [string]$websockifyjs = Join-Path $($websockifyfolder) -ChildPath "other\js\websockify.js" 
+    [string]$arguments = "node $($websockifyjs) localhost:$($proxyport) $($vnc)"
+    Write-Verbose "Starting websockify proxy from port localhost:$($proxyport) to $($vnc)"
+    $websockifyprocess = Start-Process powershell -ArgumentList $arguments -PassThru 
+    If ($websockifyprocess -eq $null)
     {
-        $webpath = $PWD
+        throw "Failed to start websockify proxy"
     }
-    ElseIf (Test-Path "$($PSScriptRoot)\..\vnc.html")
-    {
-        $webpath = "$($PSScriptRoot)\..\"
-    }
-    ElseIf (Test-Path "$($PSScriptRoot)\vnc.html")
-    {
-        $webpath = $($PSScriptRoot)
-    }
-    ElseIf (Test-Path "$($PSScriptRoot)\..\share\novnc\vnc.html")
-    {
-        $webpath = "$($PSScriptRoot)\..\share\novnc\"
-    }
-    Else
-    {
-        throw [System.IO.FileNotFoundException] "Could not find vnc.html"
-    }
+    return $websockifyprocess
 }
 
 Try 
@@ -63,54 +89,24 @@ Try
         throw "Port $webport in use. Try -webport PORT"
     }
 
-    Write-Output "Installing nodejs"
-    choco install nodejs.install -y
-
-    Write-Output "Installing required nodejs modules"
-    npm install -g optimist, policyfile, ws, http-server
-
-    Write-Output "Configuring nodejs installation"
-    [Environment]::SetEnvironmentVariable("NODE_PATH", "%AppData%\npm\node_modules", "User")
-    [Environment]::SetEnvironmentVariable("Path", ("{0};{1}" -f [Environment]::GetEnvironmentVariable("Path","User"), [Environment]::GetEnvironmentVariable("NODE_PATH","User")), "User")
+    Install-NodeJs
 
     $tempfolder = New-Item -ItemType directory "$($PWD)\$(get-date -f yyyyMMddHHmmss)" 
     [string] $webfolder = Join-Path $tempfolder -ChildPath "\web"
     [string] $novncfolder = Join-Path $tempfolder -ChildPath "\web\noVNC-master"
     [string] $websockifyfolder = Join-Path $tempfolder -ChildPath "\web\websockify-master"
 
-    Write-Output "Downloading novnc git master repository; $($tempfolder)\novnc.zip"    
-    wget https://github.com/novnc/noVNC/archive/master.zip -OutFile "$($tempfolder)\novnc.zip"
-    
-    Write-Output "Extracting novnc source in $($novncfolder)"
-    Add-Type -AssemblyName "System.IO.Compression.FileSystem"    
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("$($tempfolder)\novnc.zip", "$($webfolder)")
+    Get-NoVncSource -tempfolder $tempfolder -novncfolder $novncfolder
+    Get-WebSockifySource -tempfolder $tempfolder -websockifyfolder $websockifyfolder
 
-    Write-Output "Downloading websockify git master repository; $($tempfolder)\websockify.zip"
-    wget https://github.com/novnc/websockify/archive/master.zip -OutFile "$($tempfolder)\websockify.zip"
+    $novncprocess = Start-NoVncWebServer -novncfolder $novncfolder -webport $webport
+    $websockifyprocess = Start-WebSockifyProxy -websockifyfolder $websockifyfolder -proxyport $proxyport -vnc $vnc
 
-    Write-Output "Extracting novnc source in $($websockifyfolder)"
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("$($tempfolder)\websockify.zip", "$($webfolder)")
+    [string] $novncclient = "http://localhost:$($webport)/vnc.html?host=localhost&port=$($proxyport)"
+    Sleep -Seconds 1
+    Start-Process $novncclient
 
-    [string]$arguments = "http-server $($novncfolder) -p $($webport)"
-    Write-Output "Starting novnc web server on port $($webport)"
-    $novncprocess = Start-Process powershell -ArgumentList $arguments -PassThru
-    If ($novncprocess -eq $null)
-    {
-        throw "Failed to start novnc web server"
-    }
-
-    [string]$websockifyjs = Join-Path $($websockifyfolder) -ChildPath "other\js\websockify.js" 
-    [string]$arguments = "node $($websockifyjs) localhost:$($proxyport) $($vnc)"
-    Write-Output "Starting websockify proxy from port :$($proxyport) to $($vnc)"
-    $websockifyprocess = Start-Process powershell -ArgumentList $arguments -PassThru
-    If ($websockifyprocess -eq $null)
-    {
-        throw "Failed to start websockify proxy"
-    }
-
-    Write-Output ("Navigate to this URL: http://localhost:$($webport)/vnc.html?host=localhost&port=$($proxyport)")
-
-    Wait-Process $novncprocess.Id
+    Wait-Process -InputObject $novncprocess, $websockifyprocess
 }
 Catch 
 {
@@ -121,5 +117,13 @@ Finally
     If (Test-Path $tempfolder)
     {
         Remove-Item $tempfolder -Recurse
-    }    
+    }
+    If ($novncprocess -ne $null)
+    {
+        Stop-Process $novncprocess -ErrorAction SilentlyContinue
+    }
+    If ($websockifyprocess -ne $null)
+    {
+        Stop-Process $websockifyprocess -ErrorAction SilentlyContinue
+    }
 }
